2ec4d01f62407bda68f604264b790a16a51bdea9
Shivam <singhshivamkr@gmail.com>
2020-12-06 01:04:56 +0530

    added ouch sound

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import { counter } from "./style.css";
+import ouch from "url:../public/ouch.mp3";
+
 // const isEven = (count) => {
 //   if (count % 2 === 0) {
 //     return "It's even babe";
@@ -7,17 +9,21 @@ import { counter } from "./style.css";
 //     return "It's odd";
 //   }
 // };
+
 /** return is even or odd in true false */
 const isEven = (count) => count % 2 === 0;
 
 const App = () => {
   const [count, setCount] = useState(0);
+  const audioElRef = useRef(null);
 
   const incrementCount = () => {
     setCount((prv) => {
       console.log(prv);
       return prv + 1;
     });
+    // console.log(audioElRef);
+    audioElRef.current.play();
     // console.log("You pushed");
   };
   //   if (count === 25) {
@@ -35,6 +41,7 @@ const App = () => {
         {count % 2 === 0 ? "Babua ee ta even hai" : "Ee ta odd babu hai "}
       </h3>
       <button onClick={incrementCount}>Push Me!</button>
+      <audio src={ouch} ref={audioElRef}></audio>
     </>
   );
 };
